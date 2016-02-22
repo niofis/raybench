c2572332f1956d201032b759aa0e38a83530926d
Enrique <niofis@gmail.com>
2016-02-21 22:45:42 -0700

    implemented some classes: vector, ray, sphere, camera, etc.

--- luarb.lua
+++ luarb.lua
@@ -1,4 +1,5 @@
 local io = require("io")
+local math = require("math")
 
 local WIDTH = 1280
 local HEIGHT = 720
@@ -10,15 +11,162 @@ function Vector3.new (x, y, z)
   local v = {}
   setmetatable(v, Vector3)
 
-  v.x = x
-  v.y = y
-  v.z = z
+  v.x = x || 0
+  v.y = y || 0
+  v.z = z || 0
 
   return v
 end
 
+function Vector3:__add (v)
+  local r = Vector3.new()
+
+  r.x = self.x + v.x
+  r.y = self.y + v.y
+  r.z = self.z + v.z
+
+  return r
+end
+
+function Vector3:__sub (v)
+  local r = Vector3.new()
+
+  r.x = self.x - v.x
+  r.y = self.y - v.y
+  r.z = self.z - v.z
+
+  return r
+end
+
+function Vector3:__mul (v)
+  local r = Vector3.new()
+
+  if type(v) == "table" then 
+    r.x = self.x * v.x
+    r.y = self.y * v.y
+    r.z = self.z * v.z
+  else if type(v) == "number" then
+    r.x = self.x * v
+    r.y = self.y * v
+    r.z = self.z * v
+  end
+
+  return r
+end
+
+function Vector3:__div (v)
+  local r = Vector3.new()
+
+  if type(v) == "table" then 
+    r.x = self.x / v.x
+    r.y = self.y / v.y
+    r.z = self.z / v.z
+  else if type(v) == "number" then
+    r.x = self.x / v
+    r.y = self.y / v
+    r.z = self.z / v
+  end
+
+  return r
+end
+
+function Vector3:dot (v)
+  local r =
+    self.x * v.x +
+    self.y * v.y +
+    self.z * v.z
+
+  return r
+end
+
+function Vector3:norm ()
+  local r = math.sqrt(
+    self.x * self.x +
+    self.y * self.y +
+    self.z * self.z)
+
+  return r
+end
+
+function Vector3:mkunit ()
+  local n = self.norm()
+  local r = self / n
+
+  return r
+end
+
+local Ray = {}
+Ray.__index = Ray
+
+function Ray.new (ops)
+  local r = {}
+
+  setmetatable(r, Ray)
+
+  r.origin = ops.origin || Vector3.new()
+  r.direction = ops.direction || Vector3.new()
+
+  return r
+end
+
+function Ray:point (dist)
+  local r = Vector3.new()
+
+  r.x = self.origin + self.direction * dist
+
+  return r
+end
+
+local Camera = {}
+Camera.__index = Camera
+
+function Camera.new (ops)
+  local c = {}
+
+  setmetatable(c, Camera)
+
+  c.eye = ops.eye || Vector3.new(0, 4.5, 75)
+  c.lt = ops.lt || Vector3.new(-8, 9, 50)
+  c.rt = ops.rt || Vector3.new(8, 9, 50)
+  c.lb = ops.lb || Vector3.new(-8, 0, 50)
+
+  return c
+end
 
 
+local Sphere = {}
+Sphere.__index = Sphere
+
+function Sphere.new (ops)
+  local s = {}
+
+  setmetatable(s, Sphere)
+
+  s.center = ops.center || Vector3.new()
+  s.radius = ops.radius || 1
+  s.color = ops.color || Vector3.new(1.0, 0, 0)
+  s.is_light = ops.is_light || true
+
+  return s
+end
+
+function Sphere:hit (ray)
+end
+
+local Hit = {}
+Hit.__index = Hit
+
+function Hit.new (ops)
+  local h = {}
+
+  setmetatable(h, Hit)
+
+  h.dist = ops.dist || 0
+  h.point = ops.point || Vector.new()
+  h.normal = ops.normal || Vector.new()
+
+  return h
+end
 
 local function trace (ray, depth)
 end
