f1cd0ff6b169dbe356712a28495accf02a84154e
Enrique <niofis@gmail.com>
2018-09-03 22:35:42 -0700

    continuing with c++ implementation

--- cpprb.cpp
+++ cpprb.cpp
@@ -18,8 +18,8 @@ class V3 {
     V3 operator+(const V3 &b) {
       V3 res {
         .x = this->x + b.x,
-        .y = this->y + b.y,
-        .z = this->z + b.z
+          .y = this->y + b.y,
+          .z = this->z + b.z
       };
       return res;
     }
@@ -27,8 +27,8 @@ class V3 {
     V3 operator*(const V3 &b) {
       V3 res {
         .x = this->x * b.x,
-        .y = this->y * b.y,
-        .z = this->z * b.z
+          .y = this->y * b.y,
+          .z = this->z * b.z
       };
       return res;
     }
@@ -36,8 +36,8 @@ class V3 {
     V3 operator-(const V3 &b) {
       V3 res {
         .x = this->x - b.x,
-        .y = this->y - b.y,
-        .z = this->z - b.z
+          .y = this->y - b.y,
+          .z = this->z - b.z
       };
       return res;
     }
@@ -45,8 +45,8 @@ class V3 {
     V3 operator*(float s) {
       V3 res {
         .x = this->x * s,
-        .y = this->y * s,
-        .z = this->z * s
+          .y = this->y * s,
+          .z = this->z * s
       };
       return res;
     }
@@ -54,8 +54,8 @@ class V3 {
     V3 operator/(const V3 &b) {
       V3 res {
         .x = this->x / b.x,
-        .y = this->y / b.y,
-        .z = this->z / b.z
+          .y = this->y / b.y,
+          .z = this->z / b.z
       };
       return res;
     }
@@ -63,16 +63,16 @@ class V3 {
     V3 operator/(float s) {
       V3 res {
         .x = this->x / s,
-        .y = this->y / s,
-        .z = this->z / s
+          .y = this->y / s,
+          .z = this->z / s
       };
       return res;
     }
 
     float dot(const V3 &b) {
       return  this->x * b.x +
-              this->y * b.y +
-              this->z * b.z;
+        this->y * b.y +
+        this->z * b.z;
     }
 
     float norm() {
@@ -93,7 +93,7 @@ class Ray {
   public:
     V3 origin;
     V3 direction;
-    
+
     V3 point(float t) {
       return this->origin + (this->direction * t);
     }
@@ -120,23 +120,88 @@ class World {
     vector<Sphere> spheres;
     Camera camera;
 
-    World base() {
-      World res = {
-        spheres = new vector<Sphere> {
-          new Sphere {
-            .center = new V3 {.x = 0.0f, .y = -10002.0f, .z = 0.0f},
-            .radius = 9999.0f,
-            .color = new V3 {.x = 1.0f, .y = 1.0f, .z = 1.0f}
-          }
+    World() {
+      this->spheres = vector<Sphere> {
+        Sphere {
+          .center = V3 {.x = 0.0f, .y = -10002.0f, .z = 0.0f},
+          .radius = 9999.0f,
+          .color = V3 {.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        },
+        Sphere {
+          .center = V3 {.x = -10012.0f, .y = 0.0f, .z = 0.0f},
+          .radius = 9999.0f,
+          .color = V3 {.x = 1.0f, .y = 0.0f, .z = 0.0f}
+        },
+        Sphere {
+          .center = V3 {.x = 10012.0f, .y = 0.0f, .z = 0.0f},
+          .radius = 9999.0f,
+          .color = V3 {.x = 0.0f, .y = 1.0f, .z = 0.0f}
+        },
+        Sphere {
+          .center = V3 {.x = 0.0f, .y = 0.0f, .z = -10012.0f},
+          .radius = 9999.0f,
+          .color = V3 {.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        },
+        Sphere {
+          .center = V3 {.x = 0.0f, .y = 10012.0f, .z = 0.0f},
+          .radius = 9999.0f,
+          .color = V3 {.x = 1.0f, .y = 1.0f, .z = 1.0f},
+          .is_light = true
+        },
+        Sphere {
+          .center = V3 {.x = -5.0f, .y = 0.0f, .z = 2.0f},
+          .radius = 2.0f,
+          .color = V3 {.x = 1.0f, .y = 1.0f, .z = 0.0f}
+        },
+        Sphere {
+          .center = V3 {.x = 0.0f, .y = 5.0f, .z = -1.0f},
+          .radius = 4.0f,
+          .color = V3 {.x = 1.0f, .y = 0.0f, .z = 0.0f}
+        },
+        Sphere {
+          .center = V3 {.x = 8.0f, .y = 5.0f, .z = -1.0f},
+          .radius = 2.0f,
+          .color = V3 {.x = 0.0f, .y = 0.0f, .z = 1.0f}
         }
       };
-      return res;
+
+      this->camera = Camera {
+        eye = V3 {.x = 0.0f, .y = 4.5f, .z = 75.0f},
+        lt = V3 {.x = -8.0f, .y = 9.0f, .z = 50.f},
+        rt = V3 {.x = 8.0f, .y = 9.0f, .z = 50.0f},
+        lb = V3 {.x = -8.0f, .y = 0.0f, .z = 50.0f}
+      };
     }
 };
 
 int main() {
-  V3 a {.x=1,.y=2,.z=3};
-  V3 b {.x=10,.y=20,.z=30};
-  cout<<"Coso 10="<<(a+b).z<<endl;
+  World world;
+  V3 vdu = (world.camera.rt - world.camera.lt) / (float) WIDTH;
+  V3 vdu = (world.camera.lb - world.camera.lt) / (float) HEIGHT;
+
+  for(uint_fast16_t y = 0; y < HEIGHT; ++y)
+  {
+    for(uint_fast16_t x = 0; x < WIDTH; ++x)
+    {
+      Ray r = {
+        .origin = world.camera.lt
+      };
+
+      V3 u;
+      V3 v;
+      V3 c = {0};
+      
+      for(uint_fast16_t s = 0; s < SAMPLES; ++s)
+      {
+        u = vdu * ((float)x + randf());
+        v = vdv * ((float)y + randf());
+        r.direction = ((world.camera.lt + u + v) - r.origin).unit();
+        u = world.trace(r, 0);
+        c = c + u;
+        
+      }
+    }
+  }
+
   return 0;
 }
