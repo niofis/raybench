9e4dd7d4cb890b6eb18194820e7cb260c527114c
Enrique <niofis@gmail.com>
2018-11-20 23:06:47 -0700

    added xorshift rng

--- crb-omp.c
+++ crb-omp.c
@@ -219,9 +219,21 @@ bool hit_sphere(const struct sphere* sp, const struct ray* ray, struct hit* hit)
   return false;
 }
 
+//https://codingforspeed.com/using-faster-psudo-random-generator-xorshift/
+uint32_t xor128(void) {
+  static uint32_t x = 123456789;
+  static uint32_t y = 362436069;
+  static uint32_t z = 521288629;
+  static uint32_t w = 88675123;
+  uint32_t t;
+  t = x ^ (x << 11);   
+  x = y; y = z; z = w;   
+  return w = w ^ (w >> 19) ^ (t ^ (t >> 8));
+}
+
 float randf()
 {
-    return (float)rand() / (float)RAND_MAX ;
+    return (float)xor128() / (float)UINT32_MAX;
 }
 
 struct v3 rnd_dome(const struct v3* normal)
