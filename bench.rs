ee2cd387b9b8e8b3bb5a435099d1bfe3be63a93b
Enrique <niofis@gmail.com>
2019-10-14 23:23:15 -0700

    go, rust, c test running. runtimes summary

--- bench.rs
+++ bench.rs
@@ -5,31 +5,73 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::process::Command;
 
-fn baseline() {
-    println!("Compiling...");
-    Command::new("gcc")
-        .args(&["crb.c", "-o", "crb", "-std=c11", "-O3", "-lm"])
+fn compile_run(
+    name: &str,
+    compiler: &str,
+    args: Vec<&str>,
+    run: &str,
+    ppm: &str,
+) -> (String, f64, String) {
+    println!("{}\nCompiling...", &name);
+    Command::new(compiler)
+        .args(&args)
         .spawn()
         .expect("compilation did not succeed")
         .wait();
 
     println!("Running...");
     let start = time::precise_time_s();
-    let output = Command::new("./crb").output().expect("crb failed to run");
+    let output = Command::new(run).output().expect("failed to run");
     let end = time::precise_time_s();
-    println!("Baseline C running time: {:.}s", end - start);
+    let elapsed = end - start;
+    println!("Running time: {:.4}s", elapsed);
 
     let ppm_string = String::from_utf8_lossy(&output.stdout);
-    let mut ppm = BufWriter::new(File::create("baseline.ppm").expect("Error creating file"));
-    write!(ppm, "{}", ppm_string);
-    println!("Saved baseline.ppm file");
+    let mut ppm_file = BufWriter::new(File::create(ppm).expect("error creating file"));
+    write!(ppm_file, "{}", &ppm_string);
+
+    (name.to_string(), elapsed, ppm_string.to_string())
+}
+
+fn baseline() -> (String, f64, String) {
+    compile_run(
+        "Baseline (C lang)",
+        "gcc",
+        vec!["crb.c", "-o", "baseline", "-std=c11", "-O3", "-lm"],
+        "./baseline",
+        "baseline.ppm",
+    )
+}
+
+fn c_lang() -> (String, f64, String) {
+    compile_run(
+        "C",
+        "gcc",
+        vec!["crb.c", "-o", "crb", "-std=c11", "-O3", "-lm"],
+        "./crb",
+        "crb.ppm",
+    )
+}
+
+fn rust_lang() -> (String, f64, String) {
+    compile_run(
+        "Rust Alt",
+        "rustc",
+        vec!["rsrb_alt.rs", "-o", "rsrb_alt", "-O"],
+        "./rsrb_alt",
+        "rsrb_alt.ppm",
+    )
+}
+
+fn go_lang() -> (String, f64, String) {
+    compile_run("Go", "go", vec!["build", "gorb.go"], "./gorb", "gorb.ppm")
 }
 
 fn main() {
     let matches = App::new("raybench runner")
         .version("0.1")
         .author("Enrique <niofis@gmail.com>")
-        .about("Compiles runs and compares different raybench tests.\nAvailable implementations: c, rust, js, cs, python")
+        .about("Compiles runs and compares different raybench tests.\nAvailable implementations: c, rust, js, go, cs")
         .subcommand(
             SubCommand::with_name("baseline")
             .about("builds and runs the baseline C implementation")
@@ -51,7 +93,23 @@ fn main() {
         baseline();
     } else if let Some(matches) = matches.subcommand_matches("run") {
         if let Some(langs) = matches.value_of("implementations") {
-            println!("{:?}", langs);
+            let results: Vec<(String, f64, String)> = langs
+                .split(",")
+                .filter_map(|lang| {
+                    if lang == "c" {
+                        return Some(c_lang());
+                    } else if lang == "rust" {
+                        return Some(rust_lang());
+                    } else if lang == "go" {
+                        return Some(go_lang());
+                    } else {
+                        return None;
+                    }
+                })
+                .collect();
+            results
+                .iter()
+                .for_each(|(name, elapsed, _)| println!("{:7} \t {:.4}s", name, elapsed));
         }
     }
 }
